b3f2b2d29d3c23ce8f07861ff780e37a79a0125e
EduardoGubert <edugubertnascimento@gmail.com>
2026-01-30 18:21:19 -0400

    Remover geração automática de UUID para as propriedades Id nas configurações de Product, Sale, SaleItem e User

--- template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/ProductConfiguration.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/ProductConfiguration.cs
@@ -16,7 +16,7 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
         builder.HasKey(p => p.Id);
         builder.Property(p => p.Id)
             .HasColumnType("uuid")
-            .HasDefaultValueSql("gen_random_uuid()");
+            .ValueGeneratedNever();
 
         builder.Property(p => p.Title)
             .IsRequired()
